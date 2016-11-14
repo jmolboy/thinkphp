edc3e52d9cc86e3407c45a96339c39573fdf59a0
jmolboy <johnnymol@163.com>
2016-11-14 14:59:31 +0800

    去掉对于jsonp的支持
    去掉对于jsonp的支持，无这方面需求

--- ThinkPHP/Mode/Miot/Controller.class.php
+++ ThinkPHP/Mode/Miot/Controller.class.php
@@ -218,16 +218,6 @@ abstract class Controller
                 header('Content-Type:application/json; charset=utf-8');
                 $data = json_encode($data, $json_option);
                 break;
-            case 'JSONP':
-                // 返回JSON数据格式到客户端 包含状态信息
-                header('Content-Type:application/json; charset=utf-8');
-                $handler = isset($_GET[C('VAR_JSONP_HANDLER')]) ? $_GET[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
-                $data    = $handler . '(' . json_encode($data, $json_option) . ');';
-                break;
-            case 'EVAL':
-                // 返回可执行的js脚本
-                header('Content-Type:text/html; charset=utf-8');
-                break;
         }
         exit($data);
     }
