444bac7181f388cdd8fbd84d359dd0af10f2d8ec
jmolboy <johnnymol@163.com>
2016-11-14 16:48:01 +0800

    添加模式文件

--- ThinkPHP/Mode/miot.php
+++ ThinkPHP/Mode/miot.php
@@ -9,12 +9,12 @@
 // | Description:
 // +----------------------------------------------------------------------
 /**
- * ThinkPHP Lite模式定义
+ * ThinkPHP Miot模式定义
  */
 return array(
     // 配置文件
     'config' => array(
-        MODE_PATH . 'Lite/convention.php', // 系统惯例配置
+        MODE_PATH . 'Miot/convention.php', // 系统惯例配置
         CONF_PATH . 'config' . CONF_EXT, // 应用公共配置
     ),
 
@@ -30,12 +30,11 @@ return array(
 
     // 函数和类文件
     'core'   => array(
-        MODE_PATH . 'Lite/functions.php',
+        MODE_PATH . 'Miot/functions.php',
         COMMON_PATH . 'Common/function.php',
         CORE_PATH . 'Hook' . EXT,
         CORE_PATH . 'App' . EXT,
         CORE_PATH . 'Dispatcher' . EXT,
-        //CORE_PATH . 'Log'.EXT,
         CORE_PATH . 'Route' . EXT,
         CORE_PATH . 'Controller' . EXT,
         CORE_PATH . 'View' . EXT,
